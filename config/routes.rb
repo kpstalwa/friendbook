962f0af751800ceb3cdbd83e3fef8a77a9278907
Karam Talwandi <k.talwandi@hotmail.com>
2018-01-07 01:16:04 -0500

    add user index and profile

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get 'static_pages/home' => 'static_pages#home'
   root 'static_pages#home'
   devise_for :users
+  resources :users, :only => [:show, :index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
