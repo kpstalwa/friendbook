f8b98472e7dc18d235566d7b2f4e5637bd98c31f
Karam Talwandi <k.talwandi@hotmail.com>
2018-01-07 12:51:38 -0500

    remove turbo links

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'static_pages/home' => 'static_pages#home'
   root 'static_pages#home'
-  devise_for :users
+  devise_for :users, :path => 'u'
   resources :users, :only => [:show, :index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
