267b8ed3a1e8e28aa0cbb5dc6de4a4676f7a8434
Karam Talwandi <k.talwandi@hotmail.com>
2018-01-07 20:58:08 -0500

    refactor code, add profile page, friendspage

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -6,7 +6,6 @@ class FriendsController < ApplicationController
     @friends = current_user.friends
   end
   def destroy
-
      current_user.remove_friend(@friend)
      flash[:notice] = "You are no longer friends with #{@friend.name}"
      redirect_to root_path
