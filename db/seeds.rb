0846e37f1896090ba972e8720ca0732399708e51
Karam Talwandi <k.talwandi@hotmail.com>
2018-01-09 13:51:19 -0500

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,28 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+
+tourUser = User.new(name:  "Tour",
+ email: "example@railstutorial.org",
+ password:              "foobar",
+ password_confirmation: "foobar")
+tourUser.skip_confirmation!
+tourUser
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+ user = User.new(name:  name,
+    email: email,
+    password:              password,
+    password_confirmation: password)
+  user.skip_confirmation!
+  user.save!
+end
+
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Lorem.sentence(5)
+  users.each { |user| user.posts.create!(content: content) }
+end
+
